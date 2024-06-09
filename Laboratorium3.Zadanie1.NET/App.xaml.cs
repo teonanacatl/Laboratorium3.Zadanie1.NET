e7198513430d5a5d77168589031232eba5d9c493
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-09 16:43:22 +0200

    Add missing files

---
+++ Laboratorium3.Zadanie1.NET/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Laboratorium3.Zadanie1.NET;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
